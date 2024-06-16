285b5d59d54a509ea77d22a8d741c37383543f3d
dbd1 <datboicryptodev@gmail.com>
2024-06-16 19:15:48 +0000

    new page

---
+++ app/menu/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import Menu from "@components/menu";
+
+export default function Home() {
+  return <Menu />;
+}
---
+++ components/menu.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+function Menu(){
+    return(
+        <div id="menu" className="text-center min-h-[100vh] py-28 relative">
+            <div className="text-3xl max-w-[60%] mx-auto pb-8">
+                MORE TO COME SOON!                  
+            </div>
+        </div>
+    );
+}
+
+export default Menu;
