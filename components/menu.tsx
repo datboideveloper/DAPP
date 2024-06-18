b75afbe6165c4fcb5b9a99c9f9926e41d5010087
dbd1 <datboicryptodev@gmail.com>
2024-06-18 04:44:03 +0000

    stff

--- components/menu.tsx
+++ components/menu.tsx
@@ -6,7 +6,7 @@ function Menu() {
   return (
     <div id="menu" className="text-center min-h-[100vh] py-28 relative">
       <div className="text-3xl max-w-[60%] mx-auto pb-8">
-        MORE TO COME SOON!
+        <p className="fade1 pb-24 text-center">MORE TO COME SOON!</p>
         <Link href="/">Back to home</Link>
       </div>
     </div>
